3d9ec3791135de4a16478f11c0ad85743d2fce49
kim0sun <kim0sun@korea.ac.kr>
2022-03-23 16:58:45 +0900

    test

--- R/lca.test.R
+++ R/lca.test.R
@@ -3,12 +3,14 @@ data("gss08")
 
 # MLCA
 lca = glca(item(DEFECT, HLTH, RAPE, POOR, SINGLE, NOMORE) ~ 1,
-           data = gss08, nclass = 3, n.init = 1, maxiter = 5)
+           data = gss08, nclass = 2, n.init = 1, maxiter = 1)
 par1 = lca$param$gamma
 par2 = unlist(sapply(lca$param$rho[[1]], t))
 
-pst = lcaf(t(as.matrix(lca$datalist$y[[1]])), rep(2, 6), 3, par1, par2)
-
+pst = lcaf(t(as.matrix(lca$datalist$y[[1]])), rep(2, 6), 2, par1, par2)
+round(t(exp(pst))[1:10,], 3)
+round(lca$posterior[[1]][1:10,], 3)
+lca$gof$loglik
 
 # MLCA
 lca = glca(item(DEFECT, HLTH, RAPE, POOR, SINGLE, NOMORE) ~ 1, DEGREE,
